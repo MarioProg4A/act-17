720eaafed470daec71ee7d7ccc3807de58b8c90e
xXmario fuentes Xx <160526473+MarioProg4A@users.noreply.github.com>
2024-06-15 01:16:05 +0000

    Initial commit

---
+++ README.txt
@@ -0,0 +1,9 @@
+Inicio 
+declarar las siguientes variables
+Mostrar: Mario Alberto Fuentes Linares
+Mostrar: Colonia: Morelos  
+Mostrar: Calle: hidalgo
+Mostrar: No de Casa: 358 
+Mostrar: Muicipio:aguas calientes   
+Mostrar: No de telefono: (449) 971 8484
+Fin
---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <?php echo '<p>Mario Alberto Fuentes Linares</p>'; ?> 
+    <?php echo '<p>hidalgo</p>'; ?> 
+    <?php echo '<p>358</p>'; ?> 
+    <?php echo '<p>Morelos</p>'; ?> 
+    <?php echo '<p>Aguascalientes14</p>'; ?> 
+    <?php echo '<p>(449) 971 8484</p>'; ?> 
+</html>
